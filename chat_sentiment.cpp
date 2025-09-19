e864d071db0004a79e0fe5d993ce3a493860032d
B <BalaChandraReddy.B@sony.com>
2025-09-19 12:27:55 +0530

    commiting from local

--- chat_sentiment.cpp
+++ chat_sentiment.cpp
@@ -4,36 +4,41 @@
 #include <vector>
 // using namespace std;
 
-int main() {
+int main()
+{
     // Define positive and negative keywords
-    std::vector<std::string>positive ={
-        "good", "happy", "great", "love", "nice", "awesome"
-    };
-    std::vector<std::string>negative ={
-        "bad", "sad", "angry", "hate", "worst", "ugly"
-    };
+    std::vector<std::string> positive = {
+        "good", "happy", "great", "love", "nice", "awesome"};
+    std::vector<std::string> negative = {
+        "bad", "sad", "angry", "hate", "worst", "ugly"};
 
     std::cout << "------ Chat Session Started ------" << std::endl;
     std::cout << "Type 'end' anytime to stop the chat." << std::endl;
 
     std::string msg;
     int posCount = 0, negCount = 0;
-    int turn = 1;  // 1 = User1, 2 = User2
+    int turn = 1; // 1 = User1, 2 = User2
 
-    while (true) {
+    while (true)
+    {
         std::cout << "User" << turn << ": ";
         getline(std::cin, msg);
 
-        if (msg == "end" || msg == "END") break;  // stop chat
+        if (msg == "end" || msg == "END")
+            break; // stop chat
 
         // Check for positive/negative keywords
-        for (auto &word : positive) {
-            if (msg.find(word) != std::string::npos) {
+        for (auto &word : positive)
+        {
+            if (msg.find(word) != std::string::npos)
+            {
                 posCount++;
             }
         }
-        for (auto &word : negative) {
-            if (msg.find(word) != std::string::npos) {
+        for (auto &word : negative)
+        {
+            if (msg.find(word) != std::string::npos)
+            {
                 negCount++;
             }
         }
