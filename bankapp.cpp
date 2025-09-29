6d8d7c7da46012a6d7bc4b1af885dd054ce1ac6a
B <BalaChandraReddy.B@sony.com>
2025-09-29 14:12:49 +0530

    commit

--- bankapp.cpp
+++ bankapp.cpp
@@ -14,7 +14,7 @@ class BankAccount {
             return;
         }
         balance += amount;
-        std::cout << "Deposited: $" << amount
+        std::cout << "Deposited : $" << amount
                     << " -> balance: $" << balance << std::endl;
     }
 
