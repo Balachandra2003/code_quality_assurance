9060a83a12494f889da47d53a8351f12c5b1ad30
B <BalaChandraReddy.B@sony.com>
2025-10-08 10:03:21 +0530

    commit java codes

---
+++ java/src/main/java/com/qualitycheck/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.qualitycheck;
+
+//package com.qualitycheck;
+
+/**
+ * A simple Java program that prints "Hello, World!" to the console.
+ */
+public class HelloWorld {
+
+    /**
+     * The main method is the entry point of the program.
+     *
+     * @param args Command-line arguments (not used).
+     */
+    public static void main(String[] args) {
+        // Print the message to the console
+        System.out.println("Hello, World!");
+    }
+}
