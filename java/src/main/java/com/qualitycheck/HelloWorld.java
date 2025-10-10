a7e72bfaf0b93ded51fc83f6c6b2bf65a868ff01
B <BalaChandraReddy.B@sony.com>
2025-10-10 16:46:46 +0530

    PMD commit

--- java/src/main/java/com/qualitycheck/HelloWorld.java
+++ java/src/main/java/com/qualitycheck/HelloWorld.java
@@ -1,19 +1,15 @@
 package com.qualitycheck;
 
-//package com.qualitycheck;
-
-/**
- * A simple Java program that prints "Hello, World!" to the console.
- */
 public class HelloWorld {
-
-    /**
-     * The main method is the entry point of the program.
-     *
-     * @param args Command-line arguments (not used).
-     */
     public static void main(String[] args) {
-        // Print the message to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello, PMD test!");
+    }
+
+    public void longMethod() {
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
