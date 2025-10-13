f9a21cd471940a017a9838f5098cda91227b40f4
B <BalaChandraReddy.B@sony.com>
2025-10-13 15:29:23 +0530

    chekcing1

--- java/src/main/java/com/qualitycheck/HelloWorld.java
+++ java/src/main/java/com/qualitycheck/HelloWorld.java
@@ -6,8 +6,7 @@ public class HelloWorld {
     }
 
     public void longMethod() {
-        int sum =0;
-        int x=10;
+        int sum = 0;
         for (int i = 0; i < 100; i++) {
             sum += i;
         }
