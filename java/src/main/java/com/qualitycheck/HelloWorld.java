4a0edf479fd17751ff590d4e95e50a079632dfba
B <BalaChandraReddy.B@sony.com>
2025-10-14 10:20:28 +0530

    checking pmd 5

--- java/src/main/java/com/qualitycheck/HelloWorld.java
+++ java/src/main/java/com/qualitycheck/HelloWorld.java
@@ -1,11 +1,13 @@
 package com.qualitycheck;
 
-public class HelloWorld {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        System.out.println(a + b);
-    }
+import java.util.logging.Logger;
 
-    
+public class HelloWorld {
+    private static final Logger LOGGER = Logger.getLogger(HelloWorld.class.getName());
+
+    public static void main(String[] args) {
+        final int firstNumber = 10;
+        final int secondNumber = 20;
+        LOGGER.info("Sum: " + (firstNumber + secondNumber));
+    }
 }
