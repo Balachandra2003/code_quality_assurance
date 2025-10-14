8e069232df0affc8f46f0dcf515f7498bf9163dd
B <BalaChandraReddy.B@sony.com>
2025-10-14 10:02:54 +0530

    checking 3

--- java/src/main/java/com/qualitycheck/HelloWorld.java
+++ java/src/main/java/com/qualitycheck/HelloWorld.java
@@ -2,7 +2,9 @@ package com.qualitycheck;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, PMD test!");
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
     }
 
     
