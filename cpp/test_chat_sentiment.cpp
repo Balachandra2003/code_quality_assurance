5c99588392b6084428dbd4aeb1b09888668156f9
B <BalaChandraReddy.B@sony.com>
2025-10-23 18:20:32 +0530

    fixed cpplint issue

--- cpp/test_chat_sentiment.cpp
+++ cpp/test_chat_sentiment.cpp
@@ -1,5 +1,8 @@
+// copyright 2025 Bala
 #include <gtest/gtest.h>
-#include "chat_logic.h"
+#include <string>
+#include <vector>
+#include "cpp/chat_logic.h"
 
 TEST(SentimentTest, PositiveMessages) {
     std::vector<std::string> msgs = {"I love this", "good job"};
