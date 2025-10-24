c018f9f4d50de3480032c9d4f1f97de49320aa70
B <BalaChandraReddy.B@sony.com>
2025-10-24 11:05:15 +0530

    checking gtest 2

--- cpp/test_chat_sentiment.cpp
+++ cpp/test_chat_sentiment.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 #include <string>
 #include <vector>
-#include "cpp/chat_logic.h"
+#include "chat_logic.h"
 
 TEST(SentimentTest, PositiveMessages) {
     std::vector<std::string> msgs = {"I love this", "good job"};
